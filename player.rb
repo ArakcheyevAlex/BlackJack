8367dfc3a2553b85078b0bd48b72f88e9be380c9
Alex <arakcheyev.alex@gmail.com>
2019-08-30 16:13:46 +0700

    Add Player class

---
+++ player.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class Player
+  attr_reader :name, :scores
+  attr_accessor :money
+
+  FULL_HAND_CARDS_LIMIT = 3
+
+  def initilize(name, money)
+    @name = name
+    @money = money
+    @cards = []
+  end
+
+  def drop_cards
+    self.cards = []
+  end
+
+  def take_card(card)
+    card_value = card.price
+    card_value = 1 if (@scores + card_value > 21) && card.ace?
+    @scores += card_value
+    cards << card
+  end
+
+  def cards_count
+    cards.size
+  end
+
+  def full_hand?
+    cards.size >= FULL_HAND_CARDS_LIMIT
+  end
+
+  def no_money?
+    money.zero?
+  end
+
+  private
+
+  attr_accessor :cards
+end
