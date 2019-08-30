ef7ccdd5136db5dc902685bbf5ac8d82d6716cf5
Alex <arakcheyev.alex@gmail.com>
2019-08-30 12:28:14 +0700

    Add UserInterface class

---
+++ spec/user_interface_spec.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+user_interface_spec.rb
---
+++ user_interface.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class UserInterface
+  def player_name
+    puts 'Enter your name:'
+    gets.chomp.to_s
+  end
+
+  def main_menu
+    puts '1. Hit',
+         '2. Stand',
+         '3. Open cards'
+    puts 'Enter your choice:'
+    choice = gets.chomp.to_i
+    case choice
+    when 1 then return :hit
+    when 2 then return :stand
+    when 3 then return :open
+    end
+    nil
+  end
+
+  def play_again
+    puts "Play again? 'y' or 'n'"
+    choice = gets.chomp.to_s
+    return true if choice == 'y'
+    return false if choice == 'n'
+
+    false
+  end
+
+  def win(name)
+    puts "#{name} wins!!!"
+  end
+
+  def push
+    puts 'Push'
+  end
+
+  def no_money
+    puts 'Game finished. You have no money.'
+  end
+end
