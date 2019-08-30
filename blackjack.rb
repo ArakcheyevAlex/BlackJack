62c4312653a91c330d5e1095cade805c0ce899dc
Alex <arakcheyev.alex@gmail.com>
2019-08-30 17:57:35 +0700

    Fix rubocop offenses in BlackJack

--- blackjack.rb
+++ blackjack.rb
@@ -1,24 +1,22 @@
-class BlackJack
+# frozen_string_literal: true
 
+class BlackJack
   START_MONEY_VALUE = 100
 
-  def initialize()
+  def initialize
     @ui = UserInterface.new
     player_name = ui.player_name
     @player = Player.new(player_name, START_MONEY_VALUE)
     @dealer = Player.new('Dealer', START_MONEY_VALUE)
-    @deck = Deck.new()
+    @deck = Deck.new
     @deck.shuffle!
     @bets = 0
   end
 
-  def play
-
-  end
+  def play; end
 
   private
 
-  attr_reader :ui, :player,
+  attr_reader :ui, :player
   attr_accessor :dealer, :deck, :bets
-
 end
